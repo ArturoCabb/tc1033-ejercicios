9b010a22658d11e3b292c5b1e8c3e8930193da63
ArturoCabb <arturcabbb@outlook.com>
2019-12-05 08:06:02 -0600

    Implementación del main

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,16 +1,23 @@
 #-*- coding: utf-8 -*-
 
-def greet(_mombre):
-    print("Hello ", _nombre)
+def greet(value1, value2):
+    print("Hello ", value1 + " " + value2)
 
-def run(numero):
+def run(a, b, c, d, e):
     if numero >= 0 and numero <=10:
+        x = 0
         while x < numero:
             print("Running")
             x = x + 1
 
-nombre = str(input("Ingresa tu nombre: "))
-greet(nombre)
-
-numero = int(input("Ingrese un número del 0 al 10: "))
-run(numero)
+if __name__ == "__main__":
+    Hansel = str(input("Ingresa tu nombre: "))
+    Gretel = str(input("Ingresa tus Apellidos: "))
+    greet(Hansel, Gretel)
+    
+    nu1 = int(input("Ingrese un número del 0 al 10: "))
+    nu2 = int(input("Ingrese un número del 0 al 10: "))
+    nu3 = int(input("Ingrese un número del 0 al 10: "))
+    nu4 = int(input("Ingrese un número del 0 al 10: "))
+    nu5 = int(input("Ingrese un número del 0 al 10: "))
+    run(nu1, num2, num3, num4, num5)
