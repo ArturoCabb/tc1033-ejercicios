3b1818611ef592e372fa948b78f00483586a0125
ArturoCabb <arturcabbb@outlook.com>
2019-12-05 08:11:59 -0600

    Implementación del perceptrón

--- ejercicio1.py
+++ ejercicio1.py
@@ -10,14 +10,24 @@ def run(a, b, c, d, e):
             print("Running")
             x = x + 1
 
+def perceptron(a, b, c, d):
+    if (a*b + c*d) > 20:
+        print(1)
+    else:
+        print(0)
+
 if __name__ == "__main__":
     Hansel = str(input("Ingresa tu nombre: "))
     Gretel = str(input("Ingresa tus Apellidos: "))
     greet(Hansel, Gretel)
     
-    nu1 = int(input("Ingrese un número del 0 al 10: "))
-    nu2 = int(input("Ingrese un número del 0 al 10: "))
-    nu3 = int(input("Ingrese un número del 0 al 10: "))
-    nu4 = int(input("Ingrese un número del 0 al 10: "))
-    nu5 = int(input("Ingrese un número del 0 al 10: "))
-    run(nu1, num2, num3, num4, num5)
+    num1 = int(input("Ingrese un número del 0 al 10: "))
+    num2 = int(input("Ingrese un número del 0 al 10: "))
+    num3 = int(input("Ingrese un número del 0 al 10: "))
+    num4 = int(input("Ingrese un número del 0 al 10: "))
+    num5 = int(input("Ingrese un número del 0 al 10: "))
+    run(num1, num2, num3, num4, num5)
+
+    perceptron(1, 5, 1, 5)
+    perceptron(1, 10, 1, 5)
+    perceptron(1, 5, 1, 10)
