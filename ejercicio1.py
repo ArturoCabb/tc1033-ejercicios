49c225c8051f94d16c86eb883e0c0daca5262022
ArturoCabb <arturcabbb@outlook.com>
2019-12-05 07:57:44 -0600

    Implementación de la función run

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,16 @@
+#-*- coding: utf-8 -*-
+
 def greet(_mombre):
     print("Hello ", _nombre)
 
-    nombre = str(input("Ingresa tu nombre: "))
-    greet(nombre)
+def run(numero):
+    if numero >= 0 and numero <=10:
+        while x < numero:
+            print("Running")
+            x = x + 1
+
+nombre = str(input("Ingresa tu nombre: "))
+greet(nombre)
+
+numero = int(input("Ingrese un número del 0 al 10: "))
+run(numero)
